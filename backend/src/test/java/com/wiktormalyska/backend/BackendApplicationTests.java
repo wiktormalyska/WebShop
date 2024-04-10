8119dc25bdb723fda8a54f5e290a78b49fc45055
wiktormalyska <wiktormalyska03@gmail.com>
2024-04-10 14:27:38 +0200

    Init fronend and backend

---
+++ backend/src/test/java/com/wiktormalyska/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiktormalyska.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
