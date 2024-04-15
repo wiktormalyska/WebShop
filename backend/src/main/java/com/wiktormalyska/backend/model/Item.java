b8022c62a76a92ebc342551cb4ee4e1f69ff17ad
wiktormalyska <wiktormalyska03@gmail.com>
2024-04-15 17:42:05 +0200

    Fixing hibernate

--- backend/src/main/java/com/wiktormalyska/backend/model/Item.java
+++ backend/src/main/java/com/wiktormalyska/backend/model/Item.java
@@ -9,7 +9,7 @@ import jakarta.persistence.*;
 @Entity
 public class Item {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     private int id;
     @Getter
